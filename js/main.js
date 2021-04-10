d5683942ec6f1a2173b5d3aaafd3d87d04a44ed6
AtlasDev <gabrielpatino2000@gmail.com>
2021-04-10 12:57:54 -0400

    Almost the grid-section(second-section), added the animation to the header for the scroll(missing the animation from the open navbar), fixed some issues.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,23 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menuBtnBrg = document.querySelector(".menu-btn_brg");
 const navbar = document.querySelector(".navbar");
+const headerWrapper = document.querySelector(".header-wrapper");
 
 let open = false;
 
+/* Events */
+
+// Mechanics for the navb=bar and the button
 menuBtn.addEventListener('click', toggle);
 
+// Change the background from the header case scolling
+document.addEventListener('scroll', changeBg);
+// Ensure that the header has the correct background
+changeBg();
+
+
+
+/* Functions */
 function toggle() {
   if (!open) {
     menuBtnBrg.classList.add("open");
@@ -17,5 +29,13 @@ function toggle() {
     navbar.classList.remove("open");
 
     open = false;
+  }
+}
+
+function changeBg() {
+  if (pageYOffset > 30) {
+    headerWrapper.classList.add("scrolled");
+  } else {
+    headerWrapper.classList.remove("scrolled");
   }
 }
