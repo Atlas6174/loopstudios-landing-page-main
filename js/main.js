8e9ea2a277e9bcb62212a6519c8dc61f5a2e1111
AtlasDev <gabrielpatino2000@gmail.com>
2021-04-12 18:40:49 -0400

    All done. After read the readme ans finish some details, it's done for frontendmentor

--- js/main.js
+++ js/main.js
@@ -43,3 +43,41 @@ function changeBg() {
     headerWrapper.classList.remove("scrolled");
   }
 }
+
+/* INTERSECTION OBSERVER */
+// Refrencing indivually the children from the first section
+const frstSection = document.querySelector(".first-section")
+			  .children[0]
+			  .children;
+// Referencing every grid-item
+const gridItems = document.querySelectorAll(".grid-item");
+
+
+// Options for the observer
+const options = {
+  root: null,
+  threshold: 0,
+  rootMargin: "0px 0px -20% 0px"
+}
+
+// Declaring observer. It add the class "observed" to the observed items
+const observer = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    console.log(entry)
+
+    if (entry.isIntersecting) {
+      entry.target.classList.add('observed');
+    }
+  })
+}, options);
+
+// Observing the image and text from first-section
+for (let i = 0; i < frstSection.length; i++) {
+  console.log(frstSection[i]);
+  observer.observe(frstSection[i]);
+}
+
+// Observing the images from the items grids
+gridItems.forEach(gridItem => {
+  observer.observe(gridItem);
+});
